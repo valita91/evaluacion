2fc4639131360c0a4a7486208177e921a31167e0
Valita Méndez <valentina.mt91@gmail.com>
2025-10-06 21:37:31 -0300

    modelo persona, migración, controladores y test

---
+++ app/Models/Persona.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class Persona extends Model
+{
+    use SoftDeletes;
+    protected $table = 'personas';
+    protected $fillable = [
+    'nombre',
+    'apellido', 
+    'telefono',
+    ];
+
+}
---
+++ database/migrations/2025_10_07_002428_create_personas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personas', function (Blueprint $table) {
+          $table->id();                             
+            $table->string('nombre', 100);            
+            $table->string('apellido', 100);          
+            $table->string('telefono', 30)->nullable(); 
+            $table->softDeletes();                  
+            $table->timestamps();                   
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personas');
+    }
+};
+
+
